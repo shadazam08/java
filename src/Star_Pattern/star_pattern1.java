df2a4fe9c4f6a61865e431f98abaae813faadb88
shadazam08 <azamshad08@gmail.com>
2022-08-30 18:06:39 +0530

    practice

--- src/Star_Pattern/star_pattern1.java
+++ src/Star_Pattern/star_pattern1.java
@@ -9,7 +9,7 @@ public class star_pattern1 {
                 System.out.print(" ");
             }
             for(int k = 0; k <= i; k++){
-                System.out.print("* ");
+                System.out.print("A ");
             }
             System.out.println();
         }
